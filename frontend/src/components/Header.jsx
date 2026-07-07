710bf4bd9f094ccbcee02c695eee247a32b6388d
Edouard Legoupil <edouard.legoupil@gmail.com>
2026-07-07 19:22:54 +0200

    add ref to api doc

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
             <h3>⚠️ Beta Version  V0.1 - for Testing Purpose - Once validated, this assistant will be integrated in the AI proposal drafting app, built by DER.  ⚠️</h3>
             <p>A Collaborative Virtual Assistant to generate Forced Displacement <b>Insights</b> carefully crafted for specific usages: Briefing Kit, Donors Proposals or Medias Post
                 <b> |</b> Sourced from <b>publicly available</b> UNHCR Statistics <a href="https://api.unhcr.org/docs/refugee-statistics.html" target="_blank" rel="noopener noreferrer">Database</a> and <a href="https://www.unhcr.org/what-we-do/data-and-publications/reports-and-publications" target="_blank" rel="noopener noreferrer">Reports</a>
-                <b> |</b> Powered by a <b>Model Context Protocol</b> AI Server and UNHCR Standard Data Visualisation Tools</p >
+                <b> |</b> Powered by a <b>Model Context Protocol</b> AI <a href="/redoc" target="_blank" rel="noopener noreferrer">Server</a> and UNHCR Standard Data Visualisation Tools</p >
 
         </header >
 
